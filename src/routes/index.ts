29ed11674343a94820c927d5f1284f9b71ab9c0c
Rashad <rashad@Rashads-MacBook-Pro.local>
2023-06-14 19:21:48 -0700

    update logout http method

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/refresh", authController.refreshUserToken);
-router.post("/logout", authController.logout);
+router.delete("/logout", authController.logout);
 
 router.get(
   "/user/:id",
